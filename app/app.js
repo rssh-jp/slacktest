0ec950a1c3081b90a6df7f43c3b01850709ffe8d
ARAUMI Teruo <araumi@gnavi.co.jp>
2017-07-13 13:37:33 +0900

    commit

--- app/app.js
+++ app/app.js
@@ -3,65 +3,128 @@ var util = require('util');
 var SlackBot = require('slackbots');
 var async = require('async');
 
-var execMessage = function(bot, data, callback){
+var global = {
+    candidates : [],
+    members : [],
+};
+
+var execMessage = function(bot, data){
     var params = {
-        icon_emoji: ':cat:'
+        icon_emoji: ':cat:',
+        name: 'testbot',
+        //testbot : true,
     };
     var user = null;
     var command = null;
+    var message = null;
 
-    var task = [];
-    // user情報の取得
-    task.push(function(next){
-        var user = bot.getUserById(data.user);
-        console.log('user : ', user);
-        next(null);
-    });
-    // commandの取得
-    task.push(function(next){
-        var str = data.text;
-        if(str == ''){
-            next('err');
-            return;
-        }
-        var colonSplit = str.split(':');
-        if(colonSplit.length < 2){
-            next('err');
-            return;
-        }
-        command = colonSplit[1];
-    });
-    // 実処理部
-    task.push(function(next){
-        switch(command){
-        case 'all':
-            console.log('++++++++++');
-            bot.postMessageToUser('rssh', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-            break;
-        default:
-            break;
-        }
-        next(null);
-    });
-    async.waterfall(task, function(error){
-        callback(error);
-    });
+    return Promise.resolve(
+    ).then(function(val){
+        // user情報の取得
+        return bot.getUserById(data.user).then(function(val){
+            user = val;
+            console.log('user : ', user);
+        });
+    }).then(function(val){
+        // commandの取得
+        return new Promise(function(resolve, reject){
+            var str = data.text;
+            if(str == null || str == ''){
+                reject('not found text');
+                return;
+            }
+            console.log('str : ', str);
+            var colonSplit = str.split(':');
+            if(colonSplit.length < 2){
+                reject('not enough arguments');
+                return;
+            }
+            command = colonSplit[1];
+            message = colonSplit[2];
+            resolve();
+        });
+    }).then(function(val){
+        // 実処理部
+        return new Promise(function(resolve, reject){
+            console.log('command : ', command);
+            switch(command){
+            case 'all':
+                sendMessageAllUser(bot, message, params).then(
+                    function(val){
+                        resolve(val);
+                    },
+                    function(err){
+                        reject(err);
+                    }
+                );
+                break;
+            default:
+                resolve();
+                break;
+            }
+        });
+    })
+    ;
+};
+
+var sendMessageAllUser = function(bot, message, params){
+    var users = null;
+    return bot.getUsers().then(function(val){
+        console.log('val : ', val);
+        users = val;
+    }).then(function(val){
+        return new Promise(function(resolve, reject){
+            var task = [];
+            for(var i=0; i<users.members.length; i++){
+                var v = users.members[i];
+                if(v.is_bot || v.updated == 0){
+                    continue;
+                }
+                if(v.name != 'rssh'){
+                    continue;
+                }
+                task.push(bot.postMessageToUser(v.name, message, params));
+            }
+            Promise.all(task).then(
+                function(v){
+                    resolve(v);
+                },
+                function(err){
+                    reject(err)
+                }
+            );
+        });
+    })
+    ;
+};
+var sendMessage = function(bot, from, to, message){
+    bot.name = from;
+
 };
 
 var main = function(){
     
     // create a bot
     var bot = new SlackBot({
-        token: 'xoxb-109978401488-Kr0Ve1192ZIwY6bZC6xFsofx', // Add a bot https://my.slack.com/services/new/bot and put the token 
-        name: 'fxxk bot'
+        //token: 'xoxb-109978401488-Kr0Ve1192ZIwY6bZC6xFsofx', // Add a bot https://my.slack.com/services/new/bot and put the token 
+        token: 'xoxb-108561500816-6dIwdKT4EB01j0ldLGpLKpp3', // Add a bot https://my.slack.com/services/new/bot and put the token 
+        name: 'アイウソン翔',
     });
     
     bot.on('start', function() {
         bot.on('message', function(data){
+            console.log('data : ', data);
             switch(data.type){
             case 'message':
-                execMessage(bot, data, function(err, res){
-                });
+                console.log('++++++++++++++++++++++++');
+                execMessage(bot, data).then(
+                    function(val){
+                        console.log('-------------------------');
+                    },
+                    function(err){
+                        console.log('err : ', err);
+                    }
+                );
                 break;
             default:
                 break;
